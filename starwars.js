ec2b7292f0995ce78a279bbee2a66824f9b580c0
Gon <gfusaro239@gmail.com>
2021-08-13 20:00:58 -0300

    subidotodo

---
+++ starwars.js
@@ -0,0 +1,14 @@
+var body = document.getElementsByTagName("body");
+
+window.addEventListener("click", function(){
+   
+    console.log("funciona el sonido");
+         var sonidito = new Audio("blaster.mp3");
+         sonidito.play();
+     
+ 
+      
+ })
+
+
+ 
